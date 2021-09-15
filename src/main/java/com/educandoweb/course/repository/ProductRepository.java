90d7461832297a4a0f8291de671ddb363c76b753
Marcel Branco <marcelsbranco@gmail.com>
2021-09-14 23:54:17 -0300

    ManyToMany

--- src/main/java/com/educandoweb/course/repository/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repository/ProductRepository.java
@@ -3,9 +3,9 @@ package com.educandoweb.course.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.Product;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
